9108f1886e1527a83c3774f003f7770f87fd60ee
alexvolnoff18 <alex.volnoff@mail.ru>
2023-09-04 20:35:55 +0300

    solution added

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -3,12 +3,19 @@ from django.db import models
 
 class Phone(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=60, null=False)
-    image = models.URLField(default=None)
+    name = models.CharField(max_length=50)
     price = models.IntegerField(default=None)
-    release_date = models.DateField(default=None)
+    image = models.URLField(default=None)
+    release_date = models.DateTimeField(default=None)
     lte_exists = models.BooleanField(default=None)
     slug = models.SlugField(default=None)
 
     def __str__(self):
-        return f'{self.id}. {self.name}'
+        return f"{self.id};" \
+               f" {self.name};" \
+               f" {self.price};" \
+               f" {self.image};" \
+               f" {self.release_date};" \
+               f" {self.lte_exists};" \
+               f" {self.slug}"
+
