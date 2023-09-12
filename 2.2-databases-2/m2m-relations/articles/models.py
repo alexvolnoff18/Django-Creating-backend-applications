600650d97d44b12a27cf934b1b129ff500abe27f
alexvolnoff18 <113288005+alexvolnoff18@users.noreply.github.com>
2023-09-12 22:31:49 +0300

    Update models.py

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -6,7 +6,9 @@ class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение', )
+    # scope = models.ManyToManyField('Tag', through='Scope', through_fields=('article', 'tag'),
+    #                                   verbose_name='Темы')
 
     class Meta:
         verbose_name = 'Статья'
@@ -18,24 +20,25 @@ class Article(models.Model):
 
 
 class Tag(models.Model):
-    
-    tag_name = models.CharField(max_length=60, unique=True, verbose_name='Тэг')
-    articles = models.ManyToManyField(Article, through='Scope')
+
+    tag_name = models.CharField(max_length=60, unique=True, verbose_name='Темы')
+    articles = models.ManyToManyField(Article, related_name='scopes', through='Scope')
 
     class Meta:
-        verbose_name = 'Тег'
-        verbose_name_plural = 'Теги'
+        verbose_name = 'Тема'
+        verbose_name_plural = 'Темы'
         ordering = ['-tag_name']
-  
+
     def __str__(self):
         return self.tag_name
 
 
 class Scope(models.Model):
-    
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name = 'scopes')
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name = 'scopes')
+
+    article = models.ForeignKey('Article', on_delete=models.CASCADE,  verbose_name='Статья')
+    tag = models.ForeignKey('Tag', on_delete=models.CASCADE, related_name='scopes', verbose_name='Тема')
     is_main = models.BooleanField(default=False, verbose_name='Основная тематика')
 
-    def __str__(self):
-        return self.is_main
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статей'
