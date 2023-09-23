8e3b0236f019ce656d1c446760244abd82859a29
alexvolnoff18 <alex.volnoff@mail.ru>
2023-09-23 13:06:32 +0300

    solution added

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -4,54 +4,38 @@ from logistic.models import Product, StockProduct, Stock
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
     class Meta:
         model = Product
         fields = '__all__'
-    # pass
 
-
-# https://github.com/Stanislov85/DZ_4_6_CRUD/blob/master/logistic/serializers.py
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
     class Meta:
         model = StockProduct
         fields = ['product', 'quantity', 'price']
-    # pass
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
     class Meta:
         model = Stock
         fields = '__all__'
 
     def create(self, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
-        # создаем склад по его параметрам
         stock = super().create(validated_data)
 
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+        for el in positions:
+            StockProduct.objects.create(stock=stock, **el)
 
         return stock
 
     def update(self, instance, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
-        # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
 
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
+        for el in positions:
+            StockProduct.objects.update_or_create(defaults={'quantity': el['quantity'], 'price': el['price']},
+                                                  product=el['product'], stock=stock)
         return stock
-
