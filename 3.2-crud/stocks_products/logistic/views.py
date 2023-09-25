803df21c5443f5ae410ea2de84a952c064eae2e1
alexvolnoff18 <113288005+alexvolnoff18@users.noreply.github.com>
2023-09-25 11:49:50 +0300

    Update views.py

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -16,7 +16,7 @@ class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['address', 'products']
+    filterset_fields = ['products']
 
 
 
